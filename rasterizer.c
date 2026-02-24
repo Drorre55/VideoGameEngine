0efd2bb4ca390c7c3cbcdb978973911b06187b83
Dror Regev <dror604604@gmail.com>
2026-02-24 19:43:06 +0200

    Added z_buffer

--- rasterizer.c
+++ rasterizer.c
@@ -1,8 +1,17 @@
 #include "rasterizer.h"
 #include "transformation_utils.h"
+#define CORNERS 3
 
 
 void rasterize_objects_to_frame(uint32_t* frame, unsigned int frame_width, unsigned int frame_height, WorldObjects* on_screen_objects) {
+	float* z_buffer = malloc(frame_width * frame_height * sizeof * z_buffer);
+	if (!z_buffer) {
+		SDL_LogError(1, "problem with allocating z_buffer");
+		return NULL;
+	}
+	for (int j = 0; j < frame_width * frame_height; j++)
+		z_buffer[j] = INFINITY;
+
 	for (int i = 0; i < on_screen_objects->num_triangles; i++) {
 		Triangle* current_triangle = on_screen_objects->triangle_objects[i];
 		
@@ -10,7 +19,7 @@ void rasterize_objects_to_frame(uint32_t* frame, unsigned int frame_width, unsig
 		_transform_point_to_pixel_space(current_triangle->corner2, frame_width, frame_height);
 		_transform_point_to_pixel_space(current_triangle->corner3, frame_width, frame_height);
 		
-		_draw_triangle(frame, frame_width, frame_height, current_triangle);
+		_draw_triangle(current_triangle, frame, z_buffer, frame_width, frame_height);
 
 		free(current_triangle->corner1);
 		free(current_triangle->corner2);
@@ -20,6 +29,7 @@ void rasterize_objects_to_frame(uint32_t* frame, unsigned int frame_width, unsig
 	}
 	free(on_screen_objects->triangle_objects);
 	free(on_screen_objects);
+	free(z_buffer);
 }
 
 void _transform_point_to_pixel_space(Point* point, unsigned int frame_width, unsigned int frame_height)
@@ -28,7 +38,7 @@ void _transform_point_to_pixel_space(Point* point, unsigned int frame_width, uns
 	point->y_coord = point->y_coord * frame_height;
 }
 
-void _draw_triangle(uint32_t* frame, unsigned int frame_width, unsigned int frame_height, Triangle* triangle)
+void _draw_triangle(Triangle* triangle, uint32_t* frame, float* z_buffer, unsigned int frame_width, unsigned int frame_height)
 {
 	Point** sorted_points = _sort_points_by_x(triangle->corner1, triangle->corner2, triangle->corner3);
 	
@@ -41,16 +51,17 @@ void _draw_triangle(uint32_t* frame, unsigned int frame_width, unsigned int fram
 
 	float corners_x[3] = { A->x_coord, B->x_coord, C->x_coord };
 	float corners_y[3] = { A->y_coord, B->y_coord, C->y_coord };
+	float corners_z[3] = { A->z_coord, B->z_coord, C->z_coord };
 	unsigned int corners_color[3][4];
 	memcpy(corners_color[0], A->color, sizeof(A->color));
 	memcpy(corners_color[1], B->color, sizeof(B->color));
 	memcpy(corners_color[2], C->color, sizeof(C->color));
 	
 	// walk from min x value and per column calc the edges pixels, then color between them
-	for (float i = A->x_coord; i <= B->x_coord; i++) {
+	for (float current_x = A->x_coord; current_x <= B->x_coord; current_x++) {
 		// get the edges of the triangle in this column
-		float AB_function = A->y_coord + AB_slope * (i - A->x_coord);
-		float AC_function = A->y_coord + AC_slope * (i - A->x_coord);
+		float AB_function = A->y_coord + AB_slope * (current_x - A->x_coord);
+		float AC_function = A->y_coord + AC_slope * (current_x - A->x_coord);
 
 		float y_coord_range[2];
 		if (AB_function <= AC_function) {
@@ -61,19 +72,25 @@ void _draw_triangle(uint32_t* frame, unsigned int frame_width, unsigned int fram
 			y_coord_range[0] = AC_function;
 			y_coord_range[1] = AB_function;
 		}
-		for (float j = y_coord_range[0]; j <= y_coord_range[1]; j++) {
-			float pixel_vec[2] = { i, j };
+		for (float current_y = y_coord_range[0]; current_y <= y_coord_range[1]; current_y++) {
+			if (!_is_in_frame((int)current_x, (int)current_y, frame_width, frame_height))
+				continue;
+			float pixel_vec[2] = { current_x, current_y };
 			
-			float* interpolation_weights = lin_interp2d(pixel_vec, corners_x, corners_y, 3);
-			unsigned int* interpolated_color = _interpolate_color(interpolation_weights, corners_color);
-			_draw_pixel(frame, frame_width, frame_height, (int)i, (int)j, interpolated_color);
+			float* interpolation_weights = lin_interp2d(pixel_vec, corners_x, corners_y, CORNERS);
+			float pixel_distance = _interpolate_distance(interpolation_weights, corners_z, CORNERS);
+			if (pixel_distance < z_buffer[(int)current_y * frame_width + (int)current_x]) {
+				z_buffer[(int)current_y * frame_width + (int)current_x] = pixel_distance;
+				unsigned int* interpolated_color = _interpolate_color(interpolation_weights, corners_color);
+				_draw_pixel(frame, frame_width, frame_height, (int)current_x, (int)current_y, interpolated_color);
+			}
 		}
 	}
 	// do the same from the mid x corner. (we can't continue the same becaue the first slope is irrelivant)
 	float BC_slope = (C->y_coord - B->y_coord) / (C->x_coord - B->x_coord);
-	for (float i = B->x_coord; i <= C->x_coord; i++) {
-		float BC_function = B->y_coord + BC_slope * (i - B->x_coord);
-		float AC_function = A->y_coord + AC_slope * (i - A->x_coord);
+	for (float current_x = B->x_coord; current_x <= C->x_coord; current_x++) {
+		float BC_function = B->y_coord + BC_slope * (current_x - B->x_coord);
+		float AC_function = A->y_coord + AC_slope * (current_x - A->x_coord);
 
 		float y_coord_range[2];
 		if (BC_function <= AC_function) {
@@ -85,17 +102,23 @@ void _draw_triangle(uint32_t* frame, unsigned int frame_width, unsigned int fram
 			y_coord_range[1] = BC_function;
 		}
 
-		for (int j = y_coord_range[0]; j <= y_coord_range[1]; j++) {
-			float pixel_vec[2] = { i, j };
+		for (float current_y = y_coord_range[0]; current_y <= y_coord_range[1]; current_y++) {
+			if (!_is_in_frame((int)current_x, (int)current_y, frame_width, frame_height))
+				continue;
+			float pixel_vec[2] = { current_x, current_y };
 
 			float* interpolation_weights = lin_interp2d(pixel_vec, corners_x, corners_y, 3);
-			unsigned int* interpolated_color = _interpolate_color(interpolation_weights, corners_color);
-			_draw_pixel(frame, frame_width, frame_height, (int)i, j, interpolated_color);
+			float pixel_distance = _interpolate_distance(interpolation_weights, corners_z, CORNERS);
+			if (pixel_distance < z_buffer[(int)current_y * frame_width + (int)current_x]) {
+				z_buffer[(int)current_y * frame_width + (int)current_x] = pixel_distance;
+				unsigned int* interpolated_color = _interpolate_color(interpolation_weights, corners_color);
+				_draw_pixel(frame, frame_width, frame_height, (int)current_x, (int)current_y, interpolated_color);
+			}
 		}
 	}
-	_draw_pixel(frame, frame_width, frame_height, (int)A->x_coord, (int)A->y_coord, A->color);
-	_draw_pixel(frame, frame_width, frame_height, (int)B->x_coord, (int)B->y_coord, B->color);
-	_draw_pixel(frame, frame_width, frame_height, (int)C->x_coord, (int)C->y_coord, C->color);
+	//_draw_pixel(frame, frame_width, frame_height, (int)A->x_coord, (int)A->y_coord, A->color);
+	//_draw_pixel(frame, frame_width, frame_height, (int)B->x_coord, (int)B->y_coord, B->color);
+	//_draw_pixel(frame, frame_width, frame_height, (int)C->x_coord, (int)C->y_coord, C->color);
 }
 
 Point** _sort_points_by_x(Point* point_a, Point* point_b, Point* point_c)
@@ -170,14 +193,23 @@ unsigned int* _interpolate_color(float weights[3], unsigned int corners_color[3]
 	return int_color;
 }
 
+float _interpolate_distance(float* weights, float* corners_z, unsigned int num_objects)
+{
+	float distance = 0;
+	for (int i = 0; i < num_objects; i++) {
+		distance += weights[i] * corners_z[i];
+	}
+	return distance;
+}
+
 void _draw_pixel(uint32_t* frame, unsigned int frame_width, unsigned int frame_height, unsigned int x, unsigned int y, unsigned int* color)
 {
-	if (!_is_in_frame(frame_width, frame_height, x, y))
+	if (!_is_in_frame(x, y, frame_width, frame_height))
 		return;
 	frame[y * frame_width + x] = rgba_to_uint32(color[0], color[1], color[2], color[3]);
 }
 
-unsigned int _is_in_frame(unsigned int frame_width, unsigned int frame_height, unsigned int x, unsigned int y)
+unsigned int _is_in_frame(unsigned int x, unsigned int y, unsigned int frame_width, unsigned int frame_height)
 {
 	return !(x < 0 || x >= frame_width || y < 0 || y >= frame_height);
 }
